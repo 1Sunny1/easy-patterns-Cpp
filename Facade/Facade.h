03f629863534ead99261df62eaa81c490be6d83d
1Sunny1 <szymon1999-13@o2.pl>
2019-11-07 14:59:25 +0100

    all done, compiling.

--- Facade/Facade.h
+++ Facade/Facade.h
@@ -5,17 +5,17 @@
 #include "Warriors.h"
 
 #include <memory>
-#include <optional>
 
 template<typename T, typename Y>
 class FightFacade final {
 public:
 	explicit FightFacade(std::unique_ptr<T> f1, std::unique_ptr<Y> f2) noexcept :
-		fighter1(std::move(f1), fighter2(std::move(f2))) { }
+		fighter1(std::move(f1)), fighter2(std::move(f2)) { }
 
 	void showLocation() const;
 	void showTotalAmountOfBetsAndValue() const;
 	void showFighters() const;
+	void showAmountOfPeople() const;
 
 private:
 	std::unique_ptr<T> fighter1;
@@ -31,14 +31,19 @@ void FightFacade<T, Y>::showLocation() const {
 template<typename T, typename Y>
 void FightFacade<T, Y>::showTotalAmountOfBetsAndValue() const {
 	betsies->totalBets();
-	std::cout << "Total bets value: " << betsies->totalBetsValue();
+	std::cout << '\n';
+	std::cout << "Total bets value: " << betsies->totalBetsValue() << '\n';
 }
 
 template<typename T, typename Y>
 void FightFacade<T, Y>::showFighters() const {
-	std::optional<T>(fighter1->showStats());
-	std::optional<Y>(fighter2->showStats());
+	fighter1->showStats();
+	std::cout << '\n';
+	fighter2->showStats();
 }
-
+template<typename T, typename Y>
+void FightFacade<T, Y>::showAmountOfPeople() const {
+	std::cout << "Total amount of people in this city: " << betsies->getAmountOfPeople();
+}
 #endif // ! FACADE_H_
 
--- Facade/Source.cpp
+++ Facade/Source.cpp
@@ -1,12 +1,21 @@
 #include <iostream>
-#include <memory>
 
 #include "Facade.h"
+
+//Episode two from "easy patterns Cpp" series: Facade
 
 auto main() -> int {
 	auto marcus = std::make_unique<Barbarian>("Marcus", 12, 54,76);
 	auto julius = std::make_unique<Gladiator>("Julius", 76, 51, 16);
-	FightFacade<Barbarian, Gladiator> fight(marcus, julius);
+	FightFacade<Barbarian, Gladiator> fight(std::move(marcus), std::move(julius));
+
+	fight.showFighters();
+	std::cout << '\n';
+	fight.showLocation();
+	std::cout << '\n';
+	fight.showAmountOfPeople();
+	std::cout << '\n';
+	fight.showTotalAmountOfBetsAndValue();
 
 	system("pause");
 	return 0;
